64613e656e468c3012dcf3a2c73ee27a4d838953
luzh <332862394@qq.com>
2021-12-28 16:43:29 +0800

    使用chai.spy监听回调函数

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ new Vue({
 });
 
 import chai from "chai";
+import spies from "chai-spies";
+chai.use(spies);
 const expect = chai.expect;
 //单元测试
 {
@@ -88,9 +90,9 @@ const expect = chai.expect;
     },
   });
   vm.$mount();
-  vm.$on("click", function () {
-    expect(1).to.eq(1);
-  });
+  let spy = chai.spy(function () {});
+  vm.$on("click", spy);
   let button = vm.$el;
   button.click();
+  expect(spy).to.have.been.called();
 }
