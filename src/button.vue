b0f70f29e73e2af0ea348bf3b52d25e0fa5fb0b7
luzh <332862394@qq.com>
2021-12-28 14:26:11 +0800

    添加icon 验证iconPosition为left或right

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,23 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{ [`icon-${iconPosition}`]: true }">
+    <svg class="icon" v-if="icon"><use :xlink:href="`#i-${icon}`" /></svg>
+    <div class="content"><slot /></div>
+  </button>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(value) {
+        return value === "left" || value === "right";
+      },
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -14,6 +28,11 @@ export default {};
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   &:hover {
     border-color: var(--border-color-hover);
   }
@@ -23,5 +42,26 @@ export default {};
   &:focus {
     outline: none;
   }
+  .icon {
+    order: 1;
+    margin-right: 0.1em;
+  }
+  .content {
+    order: 2;
+  }
+  &.icon-right {
+    .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.1em;
+    }
+    .content {
+      order: 1;
+    }
+  }
+}
+.icon {
+  width: 1em;
+  height: 1em;
 }
 </style>
